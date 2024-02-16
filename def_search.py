8e4b69ca78a71d6aff18b92b4f72913fce636289
VladiSm89 <smirnixinvn@gmail.com>
2024-02-16 15:18:28 +0500

    Добавлен вывод нескольких адресов

--- def_search.py
+++ def_search.py
@@ -2,56 +2,66 @@ import sqlite3
 
 #Функция получает информацию из базы данных
 def get_information_from_db(adres):
-    connection = sqlite3.connect('data.db')
+    with sqlite3.connect('data.db') as connection:
+        # Create cursor.
+        c = connection.cursor()
 
-    # Create cursor.
-    c = connection.cursor()
+        adres = adres.split()
+        street = adres[0].capitalize()
+        home_number = 'д.' + adres[1].upper()
+        adr = ('%' + street + '%' + home_number + '%',)
+        sql = "SELECT * FROM data WHERE LOWER(Адрес_дома) LIKE ?"
+        c.execute(sql, adr)
+        result = c.fetchall()
 
-    adres = adres.split()
-    street = adres[0].capitalize()
-    home_number = adres[1].upper()
-    adr = ('%' + street + '%' + home_number + '%',)
-    sql = "SELECT * FROM data WHERE LOWER(Адрес_дома) LIKE ?"
-    c.execute(sql, adr)
-    result = c.fetchone()
-    connection.close()
-    return result
+        if result is None:
+            return "Информация о данном адресе не найдена."
 
-def processing_of_data_for_printing(result):
-
-# Convert None to "нет данных" and float to int
-    result = ["нет данных" if el is None else el for el in result]
-    result = [int(el) if isinstance(el, float) else el for el in result]
-
-     # ВЫбираем более актуальные данные по профилактическим операциям
-    if result[19] == 'нет данных':
-        result[19], result[20], result[21] = result[22], result[23], result[24]
-        if result[22] == 'нет данных':
-            result[19], result[20], result[21] = result[25], result[26], result[27]
+        return result
 
 
-    keys = ('Адрес', 'Примечание', 'Принадлежность объекта', 'Год постройки', 'Степень огнестойкости', 'Размеры здания',
-            'Этажность', 'Количество подъездов(код домофона)', 'Количество квартир', 'Проинструктировано человек',
-            'Памятка в п/я', 'Пристрой', 'Тип отопления', 'Вход на чердак снаружи здания (лестница)',
-            'Вход на чердак внутри здания (лестница)', 'Доступ под свайное поле', 'Информационный стенд', 'Сухотруб',
-            'Ближайший ПВ №', 'Конструктивные элементы', 'Координаты')
+def processing_of_data_for_printing(results):
+    if not results:
+        return "Данные не найдены"
 
-    values = (result[0], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[19], result[20],
-    result[21], result[9], result[10], result[11], result[12], result[13], result[14], result[15], result[16],
-    result[17], result[18])
+    processed_results = []
 
-    info_adres = dict(zip(keys, values))
+    for result in results:
+        # Convert None to "нет данных" and float to int
+        result = ["нет данных" if el is None else el for el in result]
+        result = [int(el) if isinstance(el, float) else el for el in result]
 
-    non_output_list = tuple() # дабвляем ключи елементов словаря info_adres которых не должно быть в выводе
+        # Выбираем более актуальные данные по профилактическим операциям
+        if result[19] == 'нет данных':
+            result[19], result[20], result[21] = result[22], result[23], result[24]
+            if result[22] == 'нет данных':
+                result[19], result[20], result[21] = result[25], result[26], result[27]
 
-    result = '\n'.join([f'{key}: {value}' for key, value in info_adres.items() if key not in non_output_list
-                        and value != 'нет данных'])
-    return result
+        keys = ('Адрес', 'Примечание', 'Принадлежность объекта', 'Год постройки', 'Степень огнестойкости', 'Размеры здания',
+                'Этажность', 'Количество подъездов(код домофона)', 'Количество квартир', 'Проинструктировано человек',
+                'Памятка в п/я', 'Пристрой', 'Тип отопления', 'Вход на чердак снаружи здания (лестница)',
+                'Вход на чердак внутри здания (лестница)', 'Доступ под свайное поле', 'Информационный стенд', 'Сухотруб',
+                'Ближайший ПВ №', 'Конструктивные элементы', 'Координаты')
 
-#adres = input("адрес:")
+        values = (result[0], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[19], result[20],
+                  result[21], result[9], result[10], result[11], result[12], result[13], result[14], result[15], result[16],
+                  result[17], result[18])
+
+        info_address = dict(zip(keys, values))
+
+        non_output_list = tuple()  # дабвляем ключи елементов словаря info_adres которых не должно быть в выводе
+
+        processed_result = '\n'.join([f'{key}: {value}' for key, value in info_address.items() if key not in non_output_list
+                                      and value != 'нет данных'])
+        processed_results.append(processed_result)
+
+    return "\n\n".join(processed_results)
+
+
 def txt_information(adres_home):
     result = get_information_from_db(adres_home)
     return processing_of_data_for_printing(result)
 
-#print(txt_information(adres))
+
+
 
